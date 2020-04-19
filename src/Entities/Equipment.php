1fcf76948b0cd4b3a79f2a276054de68c81d8c7d
rboszko <r.boszko@b4b.net.pl>
2020-04-19 10:32:05 +0200

    "#005 - add equipment entity"

---
+++ src/Entities/Equipment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Recruitment\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="equipments")
+ */
+class Equipment
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=63)
+     */
+    protected $type;
+
+    /**
+     * @ORM\Column(type="string", length=127)
+     */
+    protected $designation;
+
+    /**
+     * @ORM\Column(type="integer", name="purchase_year")
+     */
+    protected $purchaseYear;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    protected $value;
+
+    /**
+     * @ORM\Column(type="string", length=511)
+     */
+    protected $description;
+}
