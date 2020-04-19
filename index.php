ea17f95c47b07ce6b422efbac50ddf30375475e8
rboszko <r.boszko@b4b.net.pl>
2020-04-19 11:19:17 +0200

    "#017 - add entities test into index.php"

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 require 'src/bootstrap.php';
 
-echo 'What now';
+use Recruitment\Entities\Workplace;
+
+$workplace = $entityManager->getRepository(Workplace::class)->find(1);
+$schedule = $workplace->getSchedules()->current();
+$person = $schedule->getPerson();
+$equipment = $workplace->getEquipment();
+
+echo sprintf('Workplace: %s', $workplace->getDesignation()) . "<br>";
+echo sprintf('Equipment: %s', $equipment->getDesignation()) . "<br>";
+echo sprintf('Scheduled at: %s, for %s', $schedule->getDuring(), $person->getName()) . "<br>";
