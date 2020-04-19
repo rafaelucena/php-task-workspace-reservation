bc3c957c143ee323e4b3eba7b775fd7ad652c63a
rboszko <r.boszko@b4b.net.pl>
2020-04-20 01:48:33 +0200

    "#025 - add saving method to schedule (adding flow for post)"

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ require 'src/bootstrap.php';
 use Recruitment\Run;
 
 $render = new Run($entityManager);
-$render->prepareScreen('list-schedule');
-$render->page();
+if (empty($_POST) === true) {
+    $render->prepareScreen('list-schedule');
+    $render->page();
+}
+
+$render->evaluate($_POST);
