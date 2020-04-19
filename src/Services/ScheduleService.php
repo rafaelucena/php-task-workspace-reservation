966dadd8f10cadef3b1699a4f74412449dcb602e
rboszko <r.boszko@b4b.net.pl>
2020-04-20 01:47:17 +0200

    "#025 - add saving method to schedule"

--- src/Services/ScheduleService.php
+++ src/Services/ScheduleService.php
@@ -11,6 +11,41 @@ use Recruitment\Services\BaseService;
 
 class ScheduleService extends BaseService
 {
+    public function save(array $request)
+    {
+        if (empty($request['parameters'])) {
+            return false;
+        }
+
+        preg_match('/\d{4}-\d{2}-\d{2}$/', $request['parameters'], $match);
+        $date = new \DateTime($match[0]);
+
+        preg_match('/^\d+/', $request['parameters'], $match);
+        $workplaceId = $match[0];
+
+        $person = $this->em->getRepository(Person::class)->findOneBy([
+            'name' => $request['value'],
+        ]);
+
+        if ($person === null) {
+            header('HTTP/1.0 404 Internal Server Error');
+            die;
+        }
+
+        $workplace = $this->em->getRepository(Workplace::class)->find($workplaceId);
+
+        $schedule = new Schedule();
+        $schedule->setWorkplace($workplace);
+        $schedule->setPerson($person);
+        $schedule->setDuring($date);
+        $schedule->setDescription('Maintenance');
+
+        $this->em->persist($schedule);
+        $this->em->flush();
+
+        return ['status' => 'success'];
+    }
+
     public function getAll()
     {
         $dates = ['2020-04-21', '2020-04-20', '2020-04-19'];
@@ -25,6 +60,7 @@ class ScheduleService extends BaseService
             $list[$workplace->getDesignation()] = [
                 'workplace' => $workplace->getDesignation(),
                 'equipment' => $equipment->getDesignation(),
+                'data' => $workplace->getId(),
                 'during' => null,
                 'person' => null,
             ];
@@ -39,7 +75,7 @@ class ScheduleService extends BaseService
         $first = end($dates);
         $rsm = new ResultSetMappingBuilder($this->em);
         $rsm->addRootEntityFromClassMetadata('Recruitment\Entities\Schedule', 's');
-        $sql = "SELECT s.* FROM schedules s WHERE s.during BETWEEN '$first' AND '$last' ORDER BY s.during DESC";
+        $sql = "SELECT s.* FROM schedules s WHERE s.during BETWEEN '$first 00:00:00' AND '$last 00:00:00' ORDER BY s.during DESC";
         $query = $this->em->createNativeQuery($sql, $rsm);
         $schedules = $query->getResult();
 
