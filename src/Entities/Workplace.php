09f21a5a194c18bb33e46f983dc8acb1468efec9
rboszko <r.boszko@b4b.net.pl>
2020-04-19 10:30:52 +0200

    "#004 - add workplace entity"

---
+++ src/Entities/Workplace.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Recruitment\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="workplaces")
+ */
+class Workplace
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=127)
+     */
+    protected $designation;
+
+    /**
+     * @ORM\Column(type="string", length=511)
+     */
+    protected $description;
+}
