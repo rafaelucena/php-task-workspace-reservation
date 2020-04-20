de22dab7cfe2ab5e1ed4b28f0993cb43f93d3d11
rboszko <r.boszko@b4b.net.pl>
2020-04-20 18:56:54 +0200

    "#043 - add functionality to add person (person service changes)"

--- src/Services/PersonService.php
+++ src/Services/PersonService.php
@@ -7,6 +7,43 @@ use Recruitment\Services\BaseService;
 
 class PersonService extends BaseService
 {
+    public function save(array $request)
+    {
+        if (empty($request['parameters'])) {
+            return false;
+        }
+
+        $decoded = $this->decodeParameters($request['parameters']);
+        $person = $this->em->getRepository(Person::class)->findOneBy([
+            'name' => $decoded['name'],
+        ]);
+
+        if ($person !== null) {
+            header('HTTP/1.0 404 Internal Server Error');
+            die;
+        }
+
+        $person = new Person();
+        $person->setName($decoded['name']);
+        $person->setLastname($decoded['lastname']);
+
+        $this->em->persist($person);
+        $this->em->flush();
+
+        return ['status' => 'success'];
+    }
+
+    private function decodeParameters($parameters)
+    {
+        $decoded = [];
+
+        preg_match('/^(\w+).+?(\w+)$/', $parameters, $match);
+        $decoded['lastname'] = $match[1];
+        $decoded['name'] = $match[2];
+
+        return $decoded;
+    }
+
     public function getAll()
     {
         $mapped = [];
