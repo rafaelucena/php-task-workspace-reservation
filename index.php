0f645f911a430f75fc64b8bf4ed5763f01ea56f3
rboszko <r.boszko@b4b.net.pl>
2020-04-21 02:39:55 +0200

    "#058 - add basic router"

--- index.php
+++ index.php
@@ -4,10 +4,28 @@ require 'src/bootstrap.php';
 
 use Recruitment\Run;
 
+$request = $_SERVER['REQUEST_URI'];
 $render = new Run($entityManager);
-if (empty($_POST) === true) {
-    $render->prepareScreen('list-schedule');
-    $render->page();
+
+if (empty($_POST) === false) {
+    $render->evaluate($_POST);
+    exit;
 }
 
-$render->evaluate($_POST);
+switch ($request) {
+    case '/' :
+        $render->prepareScreen('list-all');
+        break;
+    case '' :
+        $render->prepareScreen('list-all');
+        break;
+    case '/equipments' :
+        $render->prepareScreen('list-equipments');
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
+
+$render->page();
